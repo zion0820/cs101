97eee36cf2a4b4ce6258db9212b98cffc88d67ec
zion0820 <91184022+zion0820@users.noreply.github.com>
2022-03-31 17:02:21 +0800

    Create main.c

---
+++ lab3/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int my_strlen(char* s) {
+    int slen = 0;
+    while(*s++) slen++;
+    return slen;
+}
+
+int rec_strlen(char* s) {
+    if(*s++ =='\0') return 0;
+    return 1 + rec_strlen(s);
+}
+
+char* my_sort(char* s) {
+    int len = 0;
+	for(int i = 0; *(s + i) != '\0'; i++) {len++;}
+	for(int i = 0; i < len; i++) {
+		for(int j = 0; j < len - 1 - i; j++) {
+			if(*(s + j) > *(s + j + 1)) {char temp = *(s + j);
+											*(s + j) = *(s + j + 1);
+											*(s + j + 1) = temp;}
+			}
+	}
+	return s;
+}
+int main() {
+    char s[] = "IU is a girl!";
+    printf("len = %d\n", my_strlen(s));
+    printf("len = %d\n", rec_strlen(s));
+    printf("sort = %s\n", my_sort(s));
+    return 0;
+}
