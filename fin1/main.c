c60e2c2452193c4bca92229a16a35a610a367a80
zion0820 <91184022+zion0820@users.noreply.github.com>
2022-06-23 16:30:19 +0800

    Update main.c

--- fin1/main.c
+++ fin1/main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-int setMyBit(int* a, int b){
-    *a = (1<<b) | *a;
-    return *a;
+int setMyBit(int a, int b){
+    a = (1 << b) | a;
+    return a;
 }
-int clearMyBit(int* a, int b){
-    *a = ( (1<<b) - 1) & *a;
-    return *a;
+
+int clearMyBit(int a, int b){
+    a = ( (1 << b) - 1) & a;
+    return a;
 }
-int inverseMyBit(int* a, int b){
-    *a = (1<<b) ^ *a;
-    return *a;
+
+int inverseMyBit(int a, int b){
+    a = (1 << b) ^ a;
+    return a;
 }
+
 int main()
 {
     int a = 1;
     int b = 3;
     int t = a;
-    printf("setMyBit(%d, %d) = %d\n", t, b, setMyBit(&a, b));
+    printf("setMyBit(%d, %d) = %d\n", t, b, setMyBit(a, b));
     t = a;
-    printf("clearMyBit(%d, %d) = %d\n", t, b, clearMyBit(&a, b));
+    printf("clearMyBit(%d, %d) = %d\n", t, b, clearMyBit(a, b));
     t = a;
-    printf("inverseMyBit(%d, %d) = %d\n", t, b, inverseMyBit(&a, b));
+    printf("inverseMyBit(%d, %d) = %d\n", t, b, inverseMyBit(a, b));
     t = a;
     return 0;
 }
